72d6ede33b86a87c5a30fec11b7f3084a5ef6e48
ddvtechnology <ddvtechnology@gmail.com>
2026-05-13 21:24:44 +0000

    --

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -52,7 +52,7 @@ const DashboardLayout = () => {
                 <p className="text-sm text-gray-500">{user.email}</p>
               </div>
             </div>
-            <nav className="flex-1 px-4 py-6 space-y-1">
+            <nav className="flex-1 px-4 py-4 space-y-1">
               {navigation.map((item) => {
                 const Icon = item.icon;
                 return (
